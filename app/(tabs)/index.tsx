abcdda110fce4838e5ab02c4bb9d98c137e9e969
kechou <lucas.kechouri@outlook.com>
2025-07-24 21:54:00 +0200

    Initial commit of React Native project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Home() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Text style={{ fontSize: 18, color: 'black' }}>Bienvenue sur l&apos;application APHP</Text>
+    </View>
+    )
+} 
